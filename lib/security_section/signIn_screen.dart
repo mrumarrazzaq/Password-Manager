b0ed8d05bb58f6b3ea2c38781190680d225a9e17
UmarRazzaq <umar1z2e3r4o@gmail.com>
2022-08-01 18:52:11 +0500

    xUWJ1e5W User should be able to search the saved credentials

--- lib/security_section/signIn_screen.dart
+++ lib/security_section/signIn_screen.dart
@@ -4,7 +4,6 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:fluttertoast/fluttertoast.dart';
-import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:form_field_validator/form_field_validator.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:password_manager/authetication/authentication_with_google.dart';
